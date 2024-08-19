9740d9d15e3beadc46aa407998ef77edf7b50a44
Princia08 <princiaram38@yahoo.com>
2024-08-20 00:54:43 +0300

    adding link in signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {LoginPage} from "./authentification/login/login.page";
+import {InscriptionPage} from "./authentification/inscription/inscription.page";
 
 const routes: Routes = [
-  /*{
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }*/
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
 
-  { path: '', component: LoginPage }
+  { path: '', component: LoginPage },
+  { path: 'signup',
+    loadChildren: () => import('./authentification/inscription/inscription.module').then(m => m.InscriptionPageModule)
+  }
 
 ];
 @NgModule({
