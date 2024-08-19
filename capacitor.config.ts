ee329457af67066c7db82812d441335aafef0123
Princia <princiaram38@yahoo.com>
2024-08-19 16:05:18 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mbdsp10-frontoffice-ios-1297-1254-1301-1325',
+  webDir: 'www'
+};
+
+export default config;
