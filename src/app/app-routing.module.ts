0fab54b94ab49c575a098aa936673f2abe37b682
Princia08 <princiaram38@yahoo.com>
2024-08-21 21:32:41 +0300

    create page for list produit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {LoginPage} from "./authentification/login/login.page";
-import {InscriptionPage} from "./authentification/inscription/inscription.page";
 
 const routes: Routes = [
    {
--- src/app/pages/produit/produit.page.ts
+++ src/app/pages/produit/produit.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-produit',
   templateUrl: './produit.page.html',
   styleUrls: ['./produit.page.scss'],
 })
-export class ProduitPage implements OnInit {
+export class ProduitPage {
 
-  constructor() { }
+  constructor(protected router: Router) {}
 
-  ngOnInit() {
+  items = Array(10).fill(null).map((_, i) => ({
+    id: i.toString(),
+    imageSrc: 'assets/img/produits/haltere.png',
+    title: 'Haltère',
+    owner: 'John Doe',
+    date: '12-10-24'
+  }));
+
+  seeDetails(id: string) {
+    this.router.navigate(['/home/details-produit', id]);
   }
-
 }
